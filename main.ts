43a7bd85fe68a971b5bcafc956a979abf041b1a9
Luna Nyx <79994302+Cloise10@users.noreply.github.com>
2021-03-19 13:37:29 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onGesture(Gesture.LogoDown, function () {
+	
+})
+basic.showLeds(`
+    . # . . .
+    . # # # #
+    . # . # .
+    # # # # .
+    . . . # .
+    `)
+basic.showLeds(`
+    # . . . #
+    . # # # .
+    . # . # .
+    . # # # .
+    # . . . #
+    `)
+basic.showLeds(`
+    . . . # .
+    # # # # .
+    . # . # .
+    . # # # #
+    . # . . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # . # #
+    . # # # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . # . . .
+    . # # # #
+    . # . # .
+    # # # # .
+    . . . # .
+    `)
+basic.pause(200)
+basic.clearScreen()
+let numero = 0
